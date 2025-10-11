a0c85673742362865bb20f0d81521d88d4585c81
jlo2001 <Jacklowes2001@gmail.com>
2025-10-11 19:01:13 +0100

    Add command resolution and basic err handling

---
+++ commands/resolve_command.py
@@ -0,0 +1,6 @@
+def resolve_command(command):
+    if command == "":
+        return 
+    elif command == "":
+        return 
+    raise Exception
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from commands.parse_input import parse_input
+from commands.resolve_command import resolve_command
 
 def main():
     print("Initialising...")
     print("Welcome to JLo's Text RPG!")
 
     while 0<1:
-        user_input = input("what would you like to do?\n> ")
-        command, target = parse_input(user_input)
+        resolved = False
+        while resolved == False:
+            user_input = input("\nwhat would you like to do?\n> ")
+            command, target = parse_input(user_input)
+            try:
+                if target == None:
+                    resolve_command(command)()
+                elif target != None:
+                    resolve_command(command)(target)
+            except Exception:
+                print ("Sorry I didn't understand that command")
 
 main()
